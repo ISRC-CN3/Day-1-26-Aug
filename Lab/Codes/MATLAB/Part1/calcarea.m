f0ec185060c05061f82809c8357c2dbafd995333
lenfesto <43639967+lenfesto@users.noreply.github.com>
2022-10-20 17:27:25 +0100

    Delete Lab/Codes/MATLAB/Part1 directory

--- Lab/Codes/MATLAB/Part1/calcarea.m
+++
@@ -1,3 +0,0 @@
-function area = calcarea(rad)
-% This function calculates the area of a circle
-area = pi*rad*rad;
--- Lab/Codes/MATLAB/Part1/script3.m
+++
@@ -1,8 +0,0 @@
-% This script calculates the area of a circle
-% It prompts the user for the radius
-radius = input('Please enter the radius: ');
-% It then calls our previously created function to calculate the 
-% area and then prints the result
-area = calcarea(radius);
-fprintf('For a circle with a radius of %.2f,',radius)
-fprintf('the area is %.2f\n',area)
